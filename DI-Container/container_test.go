f9612a970130e8affb537c53b7c2d9b084c5b31f
KolManis <nikola.ilyin03@yandex.ru>
2025-12-24 15:26:56 +0500

    DI-Container

---
+++ DI-Container/container_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v container_test.go
+
+type UserService struct {
+	NotEmptyStruct bool
+}
+type MessageService struct {
+	NotEmptyStruct bool
+}
+
+type Container struct {
+	constructors map[string]interface{}
+}
+
+func NewContainer() *Container {
+	return &Container{
+		constructors: make(map[string]interface{}),
+	}
+}
+
+func (c *Container) RegisterType(name string, constructor interface{}) {
+	c.constructors[name] = constructor
+}
+
+func (c *Container) Resolve(name string) (interface{}, error) {
+	constructor, ok := c.constructors[name]
+	if !ok {
+		return nil, errors.New("service not found")
+	}
+
+	val := reflect.ValueOf(constructor)
+	if val.Kind() != reflect.Func {
+		return nil, errors.New("constructor must be a function")
+	}
+
+	if val.Type().NumOut() < 1 {
+		return nil, errors.New("constructor must return at least one value")
+	}
+
+	results := val.Call(nil)
+
+	return results[0].Interface(), nil
+}
+
+func TestDIContainer(t *testing.T) {
+	container := NewContainer()
+	container.RegisterType("UserService", func() interface{} {
+		return &UserService{}
+	})
+	container.RegisterType("MessageService", func() interface{} {
+		return &MessageService{}
+	})
+
+	userService1, err := container.Resolve("UserService")
+	assert.NoError(t, err)
+	userService2, err := container.Resolve("UserService")
+	assert.NoError(t, err)
+
+	u1 := userService1.(*UserService)
+	u2 := userService2.(*UserService)
+	assert.False(t, u1 == u2)
+
+	messageService, err := container.Resolve("MessageService")
+	assert.NoError(t, err)
+	assert.NotNil(t, messageService)
+
+	paymentService, err := container.Resolve("PaymentService")
+	assert.Error(t, err)
+	assert.Nil(t, paymentService)
+}
